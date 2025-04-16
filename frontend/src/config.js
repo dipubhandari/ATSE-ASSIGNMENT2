31966002aec7280e2b63aff7bdd02a7345e060a9
dipubhandari <dipendrabhandari123456@gmail.com>
2025-04-16 13:19:15 +0545

    latest commit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,2 @@
 
 export const server = 'https://mern-quiz-server-sudhir.onrender.com'
-// export const server = 'mongodb+srv://dipubhandari:.ComDipu@cluster0.va8aa0b.mongodb.net/quiz' || 'http://localhost:5000/'
-// export const server = 'http://localhost:5000'
